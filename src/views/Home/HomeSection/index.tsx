62e99fd531bc41984c25e1fd30cc90512d5df9d6
Szymon gonet <szymongonet@icloud.com>
2022-04-19 14:55:01 +0200

    Initial version

---
+++ src/views/Home/HomeSection/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function HomeSection() {
+
+    return (
+        <section></section>
+    );
+}
+
+export default HomeSection;
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header";
+import HomeSection from "./HomeSection";
+
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <HomeSection />
+        </>
+    );
+}
+
+export default Home;
